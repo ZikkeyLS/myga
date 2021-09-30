e709c03cc120d11102b72cc778e0fb29c2fcd548
ZikkeyLS <hisaha21@yandex.ru>
2021-09-30 16:12:30 +0300

    Fix dependencies

--- MygaServer/Code/Package.cs
+++ MygaServer/Code/Package.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using MessagePack;
+using System.IO;
 
 namespace MygaServer
 {
@@ -33,5 +34,25 @@ namespace MygaServer
         {
             return new Package(package.buffer);
         }
+    }
+
+    [MessagePackObject]
+    public class MyClass
+    {
+        // Key attributes take a serialization index (or string name)
+        // The values must be unique and versioning has to be considered as well.
+        // Keys are described in later sections in more detail.
+        [Key(0)]
+        public int Age { get; set; }
+
+        [Key(1)]
+        public string FirstName { get; set; }
+
+        [Key(2)]
+        public string LastName { get; set; }
+
+        // All fields or properties that should not be serialized must be annotated with [IgnoreMember].
+        [IgnoreMember]
+        public string FullName { get { return FirstName + LastName; } }
     }
 }
