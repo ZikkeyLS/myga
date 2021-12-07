438d79491f5b5a88fc01fbc5ec20f0245d1f84df
ZikkeyLS <hisaha21@yandex.ru>
2021-12-07 20:04:53 +0300

    Add server-side MPObject and MPAddon system

---
+++ MygaServer/Code/Addons/MPObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace MygaServer
+{
+    public class MPObject
+    {
+        public int? clientID { get; private set; }
+        public int id { get; private set; } = 0;
+        public List<IMPAddon> addons { get; private set; } = new List<IMPAddon>();
+
+        public virtual void Initialize(int id, int clientID = -1, IMPAddon[] addons = null)
+        {
+            this.addons.AddRange(addons);
+            Initialize(id, clientID);
+        }
+
+        public virtual void Initialize(int id, int clientID = -1, List<IMPAddon> addons = null)
+        {
+            this.addons.AddRange(addons);
+            Initialize(id, clientID);
+        }
+
+        public virtual void Initialize(int id, int clientID = -1)
+        {
+            this.id = id;
+            if (clientID >= 0)
+                this.clientID = clientID;
+        }
+
+        public T GetAddon<T>()
+        {
+            foreach(IMPAddon addon in addons)
+                if (addon is T)
+                    return (T)addon;
+
+            return default(T);
+        }
+    }
+
+    public interface IMPAddon
+    {
+        string error { get; set; }
+        bool initialised { get; set; }
+        void Intitialize(params object[] parametres);
+    }
+}
